830c137eeec149046c786ea3fe7513272210db43
moezAhmad <wyellow19@gmail.com>
2022-04-10 20:41:49 +0500

    Testimonials section

--- src/components/text-slider/text-slider.component.jsx
+++ src/components/text-slider/text-slider.component.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import "../../App.scss"
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
-import { Testimoials } from "../../layouts/landing_page/testimonials/testtimonials.component";
 import { SliderContent } from "./slider-contents/slider-content.component";
 
 export const TextSlider = ({ testimonials }) => {
--- src/pages/landing.component.jsx
+++ src/pages/landing.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import '../App.scss'
 import { LandingHeader } from "../layouts/landing_page/header/header.component";
 import { PastDesigns } from "../layouts/landing_page/past_designs/past_designs.component";
-import { Testimoials } from "../layouts/landing_page/testimonials/testtimonials.component";
+import { Testimoials } from "../layouts/landing_page/testimonials/testimonials.component";
 export class LandingPage extends React.Component{
     constructor (){
         super()
