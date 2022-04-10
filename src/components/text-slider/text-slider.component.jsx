bbdf1bdcc959813dfa575d2909e954937e987db9
moezAhmad <wyellow19@gmail.com>
2022-04-10 20:01:50 +0500

    Testimonial slider

--- src/components/text-slider/text-slider.component.jsx
+++ src/components/text-slider/text-slider.component.jsx
@@ -1,16 +1,28 @@
 import React from "react";
 import "../../App.scss"
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import { Testimoials } from "../../layouts/landing_page/testimonials/testtimonials.component";
+import { SliderContent } from "./slider-contents/slider-content.component";
 
-export class TextSlider extends React.Component{
-    constructor(){
-        super()
+export const TextSlider = ({ testimonials }) => {
 
-    }
-    render(){
-        return(
-            <div className="text-slider">
-                
-            </div>
-        )
-    }
+    return (
+
+        <div className="text-slider">
+            <Carousel
+                showArrows={true}
+                infiniteLoop={true}
+                showThumbs={false}
+                showStatus={false}
+                autoPlay={true}
+                interval={31000}
+            >
+                {testimonials.map((testimoial) => {
+                    return <SliderContent testimonial={testimoial} key={testimoial.id}/>
+                })}
+            </Carousel>
+        </div>
+    )
+
 } 
