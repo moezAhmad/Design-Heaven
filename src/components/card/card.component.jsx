49954b793173795b4ad465f305fb0dbe9e726a9f
moezAhmad <wyellow19@gmail.com>
2022-04-13 00:18:15 +0500

    Updated past design Card

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import '../../App.scss'
 
-export const Card = ({design}) => {
+export const Card = ({ design }) => {
     return (
-        <div className="card-container">
-                <img src={`https://robohash.org/${design.id}?set=set2&size=180x180`}/>
-                <h2>{design.name}</h2>
-                <p>{design.email}</p>      
+        <div className="card">
+            <img className="card-image" src={`https://robohash.org/${design.id}?set=set2&size=180x180`} />
+            <div className="card-text">
+                <h2 className="card-text--name u-margin-bottom-small">{design.name}</h2>
+                <p className="card-text--email">{design.email}</p>
+            </div>
+
         </div>
     )
 }
